efa4f482168569dd7aa315f3e1dae846f1a8641f
Brodie Ashcraft <brodiea19@gmail.com>
2023-08-10 11:18:06 -0700

    added relations between models. created seed data

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.associationproxy import association_proxy
 
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -9,21 +10,47 @@ metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
 
+
 class Company(Base):
-    __tablename__ = 'companies'
+    __tablename__ = "companies"
 
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     founding_year = Column(Integer())
 
+    freebies = relationship("Freebie", backref="company")
+    devs = association_proxy("freebies", "dev", creator=lambda dv: Freebie(dev=dv))
+
     def __repr__(self):
-        return f'<Company {self.name}>'
+        return f"<Company {self.name}>"
+
 
 class Dev(Base):
-    __tablename__ = 'devs'
+    __tablename__ = "devs"
 
     id = Column(Integer(), primary_key=True)
-    name= Column(String())
+    name = Column(String())
+
+    freebies = relationship("Freebie", backref="dev")
+    companies = association_proxy(
+        "freebies", "company", creator=lambda comp: Freebie(company=comp)
+    )
 
     def __repr__(self):
-        return f'<Dev {self.name}>'
+        return f"<Dev {self.name}>"
+
+
+class Freebie(Base):
+    __tablename__ = "freebies"
+
+    id = Column(Integer(), primary_key=True)
+    item_name = Column(String())
+    value = Column(Integer())
+    company_id = Column(Integer(), ForeignKey("companies.id"))
+    dev_id = Column(Integer(), ForeignKey("devs.id"))
+
+    def __repr__(self):
+        return f"<Freebie {self.item_name}"
+
+    def print_details(self):
+        return f"{self.dev.name} owns a {self.item_name} from {self.company.name}"
--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,59 @@
 #!/usr/bin/env python3
 
 # Script goes here!
+from models import Company, Dev, Freebie
+from faker import Faker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import random
+
+fake = Faker()
+
+engine = create_engine("sqlite:///freebies.db")
+Session = sessionmaker(bind=engine)
+session = Session()
+
+swag = [
+    "toy",
+    "sticker",
+    "hoody",
+    "cap",
+    "usb-drive",
+    "headset",
+    "backpack",
+    "mouse",
+    "keyboard",
+    "notepad",
+    "pen",
+]
+
+
+def delete_records():
+    session.query(Company).delete()
+    session.query(Dev).delete()
+    session.query(Freebie).delete()
+    session.commit()
+
+
+def create_records():
+    companies = [Company(name=fake.company()) for i in range(50)]
+    devs = [Dev(name=fake.name()) for i in range(50)]
+    freebies = [Freebie(item_name=random.choice(swag)) for i in range(50)]
+    session.add_all(companies + devs + freebies)
+    session.commit()
+    return companies, devs, freebies
+
+
+def relate(companies, devs, freebies):
+    for free in freebies:
+        free.dev = random.choice(devs)
+        free.company = random.choice(companies)
+
+    session.add_all(freebies)
+    session.commit()
+    return companies, devs, freebies
+
+
+delete_records()
+companies, devs, freebies = create_records()
+companies, devs, freebies = relate(companies, devs, freebies)
